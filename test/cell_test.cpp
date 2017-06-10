cbd19c1fe34e5e7588e6c56e7f512ccf2b293a49
Alex <info@alexallmont.com>
2017-06-10 01:02:03 +0100

    Added unit tests to confirm cell compass points are correctly returned

--- test/cell_test.cpp
+++ test/cell_test.cpp
@@ -11,3 +11,39 @@ TEST_CASE("a lone cell has no neighbours", "[cell]")
   REQUIRE(cell->south() == nullptr);
   REQUIRE(cell->west() == nullptr);
 }
+
+TEST_CASE("a cell correctly accesses it's internal neighbours", "[cell]")
+{
+  maze::Grid grid(2, 2);
+  maze::Cell* cell = grid.cell_at(0, 0);
+
+  REQUIRE(grid.cell_at(0, 0)->east() == grid.cell_at(0, 1));
+  REQUIRE(grid.cell_at(0, 0)->south() == grid.cell_at(1, 0));
+
+  REQUIRE(grid.cell_at(0, 1)->west() == grid.cell_at(0, 0));
+  REQUIRE(grid.cell_at(0, 1)->south() == grid.cell_at(1, 1));
+
+  REQUIRE(grid.cell_at(1, 0)->east() == grid.cell_at(1, 1));
+  REQUIRE(grid.cell_at(1, 0)->north() == grid.cell_at(0, 0));
+
+  REQUIRE(grid.cell_at(1, 1)->west() == grid.cell_at(1, 0));
+  REQUIRE(grid.cell_at(1, 1)->north() == grid.cell_at(0, 1));
+}
+
+TEST_CASE("a cell correctly reports external neighbours as invalid", "[cell]")
+{
+  maze::Grid grid(2, 2);
+  maze::Cell* cell = grid.cell_at(0, 0);
+
+  REQUIRE(grid.cell_at(0, 0)->west() == nullptr);
+  REQUIRE(grid.cell_at(0, 0)->north() == nullptr);
+
+  REQUIRE(grid.cell_at(0, 1)->east() == nullptr);
+  REQUIRE(grid.cell_at(0, 1)->north() == nullptr);
+
+  REQUIRE(grid.cell_at(1, 0)->west() == nullptr);
+  REQUIRE(grid.cell_at(1, 0)->south() == nullptr);
+
+  REQUIRE(grid.cell_at(1, 1)->east() == nullptr);
+  REQUIRE(grid.cell_at(1, 1)->south() == nullptr);
+}
