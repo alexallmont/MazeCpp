dd20a218e2531e7877081da09db573c8bd7c56be
Alex <info@alexallmont.com>
2017-06-12 00:36:20 +0100

    Added random cell selection tests and reformatted existing tests to be in line with rest of code

--- test/cell_test.cpp
+++ test/cell_test.cpp
@@ -1,8 +1,7 @@
 #include "catch.hpp"
 #include "maze/graph/grid2d.hpp"
 
-TEST_CASE("a lone cell has no neighbours", "[cell]")
-{
+TEST_CASE("a lone cell has no neighbours", "[cell]") {
   maze::Grid2d<1, 1> grid;
   auto cell = grid.cell_at(0, 0);
 
@@ -12,36 +11,50 @@ TEST_CASE("a lone cell has no neighbours", "[cell]")
   REQUIRE(cell.west().valid() == false);
 }
 
-TEST_CASE("a cell correctly accesses it's internal neighbours", "[cell]")
-{
+TEST_CASE("a cell correctly accesses it's internal neighbours", "[cell]") {
   maze::Grid2d<2, 2> grid;
 
-  REQUIRE(grid.cell_at(0, 0).east() == grid.cell_at(0, 1));
-  REQUIRE(grid.cell_at(0, 0).south() == grid.cell_at(1, 0));
+  SECTION("north west cell has valid neighbours") {
+    REQUIRE(grid.cell_at(0, 0).east() == grid.cell_at(0, 1));
+    REQUIRE(grid.cell_at(0, 0).south() == grid.cell_at(1, 0));
+  }
 
-  REQUIRE(grid.cell_at(0, 1).west() == grid.cell_at(0, 0));
-  REQUIRE(grid.cell_at(0, 1).south() == grid.cell_at(1, 1));
+  SECTION("north east cell has valid neighbours") {
+    REQUIRE(grid.cell_at(0, 1).west() == grid.cell_at(0, 0));
+    REQUIRE(grid.cell_at(0, 1).south() == grid.cell_at(1, 1));
+  }
 
-  REQUIRE(grid.cell_at(1, 0).east() == grid.cell_at(1, 1));
-  REQUIRE(grid.cell_at(1, 0).north() == grid.cell_at(0, 0));
+  SECTION("south west cell has valid neighbours") {
+    REQUIRE(grid.cell_at(1, 0).east() == grid.cell_at(1, 1));
+    REQUIRE(grid.cell_at(1, 0).north() == grid.cell_at(0, 0));
+  }
 
-  REQUIRE(grid.cell_at(1, 1).west() == grid.cell_at(1, 0));
-  REQUIRE(grid.cell_at(1, 1).north() == grid.cell_at(0, 1));
+  SECTION("south east cell has valid neighbours") {
+    REQUIRE(grid.cell_at(1, 1).west() == grid.cell_at(1, 0));
+    REQUIRE(grid.cell_at(1, 1).north() == grid.cell_at(0, 1));
+  }
 }
 
-TEST_CASE("a cell correctly reports external neighbours as invalid", "[cell]")
-{
+TEST_CASE("a cell correctly reports external neighbours as invalid", "[cell]") {
   maze::Grid2d<2, 2> grid;
 
-  REQUIRE(grid.cell_at(0, 0).west().valid() == false);
-  REQUIRE(grid.cell_at(0, 0).north().valid() == false);
+  SECTION("north west cell reports external neighbours as invalid") {
+    REQUIRE(grid.cell_at(0, 0).west().valid() == false);
+    REQUIRE(grid.cell_at(0, 0).north().valid() == false);
+  }
 
-  REQUIRE(grid.cell_at(0, 1).east().valid() == false);
-  REQUIRE(grid.cell_at(0, 1).north().valid() == false);
+  SECTION("north east cell reports external neighbours as invalid") {
+    REQUIRE(grid.cell_at(0, 1).east().valid() == false);
+    REQUIRE(grid.cell_at(0, 1).north().valid() == false);
+  }
 
-  REQUIRE(grid.cell_at(1, 0).west().valid() == false);
-  REQUIRE(grid.cell_at(1, 0).south().valid() == false);
+  SECTION("south west cell reports external neighbours as invalid") {
+    REQUIRE(grid.cell_at(1, 0).west().valid() == false);
+    REQUIRE(grid.cell_at(1, 0).south().valid() == false);
+  }
 
-  REQUIRE(grid.cell_at(1, 1).east().valid() == false);
-  REQUIRE(grid.cell_at(1, 1).south().valid() == false);
+  SECTION("south west cell reports external neighbours as invalid") {
+    REQUIRE(grid.cell_at(1, 1).east().valid() == false);
+    REQUIRE(grid.cell_at(1, 1).south().valid() == false);
+  }
 }
--- test/grid_test.cpp
+++ test/grid_test.cpp
@@ -1,36 +1,58 @@
 #include "catch.hpp"
 #include "maze/graph/grid2d.hpp"
 
-TEST_CASE("a grid correctly reports its row and column sizes", "[grid]")
-{
+TEST_CASE("a grid correctly reports its row and column sizes", "[grid]") {
   maze::Grid2d<3, 4> grid;
   REQUIRE(grid.rows() == 3);
   REQUIRE(grid.columns() == 4);
 }
 
-TEST_CASE("an empty grid is valid but its cells are not", "[grid]")
-{
+TEST_CASE("an empty grid is valid but its cells are not", "[grid]") {
   maze::Grid2d<0, 0> grid;
-  SECTION("querying a cell yields an invalid result")
-  {
+  SECTION("querying a cell yields an invalid result") {
     REQUIRE(grid.cell_at(0, 0).valid() == false);
   }
 
-  SECTION("the rows and columns are zero")
-  {
+  SECTION("the rows and columns are zero") {
     REQUIRE(grid.rows() == 0);
     REQUIRE(grid.columns() == 0);
   }
 }
 
-TEST_CASE("a non-empty grid containts cells", "[grid]")
-{
+TEST_CASE("a non-empty grid containts cells", "[grid]") {
   maze::Grid2d<2, 3> grid;
-  for (int r = 0; r < grid.rows(); ++r)
-  {
-    for (int c = 0; c < grid.columns(); ++c)
-    {
+  for (int r = 0; r < grid.rows(); ++r) {
+    for (int c = 0; c < grid.columns(); ++c) {
       REQUIRE(grid.cell_at(r, c).valid() == true);
     }
+  }
+}
+
+TEST_CASE("randomly selected cells are valid", "[grid]") {
+  maze::Grid2d<10, 10> grid;
+  size_t valid_count = 0;
+
+  const size_t ITERATIONS = 1000;
+  for (size_t i = 0; i < ITERATIONS; ++i) {
+    if (grid.random_cell().valid()) {
+      ++valid_count;
+    }
+  }
+  REQUIRE(valid_count == ITERATIONS);
+}
+
+TEST_CASE("randomly selected cells have a reasonable distribution", "[grid]") {
+  maze::Grid2d<4, 4> grid;
+  std::array<int, grid.size()> hit_per_cell = {};
+
+  const size_t ITERATIONS = 1000;
+  for (size_t i = 0; i < ITERATIONS; ++i) {
+    ++hit_per_cell[grid.random_cell().index()];
+  }
+
+  const size_t TOLERANCE = 1000 / grid.size();
+  for (size_t i = 0; i < grid.size() - 1; ++i) {
+    int diff = abs(hit_per_cell[i] - hit_per_cell[i + 1]);
+    REQUIRE(diff < TOLERANCE);
   }
 }
